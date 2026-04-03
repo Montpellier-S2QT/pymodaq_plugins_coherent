9d25782aca12812a9fd34cdd70df76982c5babd6
CitizenErase <tguyot@lcvn13.local>
2026-04-03 14:54:38 +0200

    Chameleon module tested and working

--- src/pymodaq_plugins_coherent/daq_move_plugins/daq_move_Chameleon.py
+++ src/pymodaq_plugins_coherent/daq_move_plugins/daq_move_Chameleon.py
@@ -1,5 +1,6 @@
 
 from typing import Union, List, Dict
+from time import sleep
 from pymodaq.control_modules.move_utility_classes import (DAQ_Move_base, comon_parameters_fun,
                                                           main, DataActuatorType, DataActuator)
 
@@ -11,71 +12,77 @@ from pymodaq_plugins_coherent.hardware.utils import get_resources
 
 
 class DAQ_Move_Chameleon(DAQ_Move_base):
-    """ Instrument plugin class for an actuator.
+    """ Instrument plugin class for an actuator that controls the Chameleon series of laser sources using VISA
+    communication
+        * Tested with a Coherent Chameleon Ultra II
+        * Tested with PyMoDAQ ver5.2.0a1
+        * Tested on OS: Win11
+        * NI-VISA library is required to communicate with the instrument
     
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Move module through inheritance via
     DAQ_Move_base. It makes a bridge between the DAQ_Move module and the Python wrapper of a particular instrument.
-
-    TODO Complete the docstring of your plugin with:
-        * The set of controllers and actuators that should be compatible with this instrument plugin.
-        * With which instrument and controller it has been tested.
-        * The version of PyMoDAQ during the test.
-        * The version of the operating system.
-        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
 
     Attributes:
     -----------
     controller: object
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
-         
-    # TODO add your particular attributes here if any
 
     """
-    is_multiaxes = False  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
-    _axis_names: Union[List[str], Dict[str, int]] = ['Wavelength']  # TODO for your plugin: complete the list
-    _controller_units: Union[str, List[str]] = 'nm'  # TODO for your plugin: put the correct unit here, it could be
-    # TODO  a single str (the same one is applied to all axes) or a list of str (as much as the number of axes)
-    _epsilon: Union[float, List[float]] = 0.1  # TODO replace this by a value that is correct depending on your controller
-    # TODO it could be a single float of a list of float (as much as the number of axes)
+    is_multiaxes = False
+    _axis_names: Union[List[str], Dict[str, int]] = ['Wavelength']
+    _controller_units: Union[str, List[str]] = 'nm'
+    _epsilon: Union[float, List[float]] = 0.1
 
 
     params = [{'title': 'Address:', 'name': 'address', 'type': 'list', 'limits': get_resources()},
                 {'title': 'Laser Settings:', 'name': 'laser_settings', 'type': 'group', 'expanded': True,
                'children': [
-                    {'title': 'Laser state', 'name': 'laser_state', 'type': 'list', 'limits': ['ON', 'OFF'],
+                    {'title': 'Laser state:', 'name': 'laser_state', 'type': 'list', 'limits': ['ON', 'OFF'],
                         'readonly': False},
-                    {'title': 'Wavelength (nm):', 'name': 'laser_wl', 'type': 'float',
-                    'readonly': False},
-                    {'title': 'Power (mW):', 'name': 'laser_power', 'type': 'float',
-                        'readonly': True},
-                   {'title': 'Power reading:', 'name': 'update_power', 'type': 'bool_push', 'value': False,
-                    'label': 'Update!'},
                     {'title': 'Shutter:', 'name': 'shutter_state', 'type': 'list', 'limits': ['OPEN', 'CLOSED'],
                         'readonly': False},
+                    {'title': 'Wavelength (nm):', 'name': 'laser_wl', 'type': 'int',
+                        'readonly': False},
+                    {'title': 'Power (mW):', 'name': 'laser_power', 'type': 'int',
+                        'readonly': True},
+                    {'title': 'Alignment mode:', 'name': 'align', 'type': 'group', 'expanded': False,
+                    'children': [
+                        {'title': 'Toggle alignment mode:', 'name': 'align_mode', 'type': 'bool',
+                            'readonly': False},
+                        {'title': 'Alignment mode available power (mW):', 'name': 'align_power', 'type': 'int',
+                            'readonly': True},
+                        {'title': 'Alignment mode wavelength (nm):', 'name': 'align_wl', 'type': 'int',
+                            'readonly': True}
+                        ]},
+                   {'title': 'Update settings:', 'name': 'update_settings', 'type': 'bool_push', 'value': False,
+                    'label': 'Update!'},
                     ]},
                 ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
-    # _epsilon is the initial default value for the epsilon parameter allowing pymodaq to know if the controller reached
-    # the target value. It is the developer responsibility to put here a meaningful value
 
 
     def ini_attributes(self):
         self.controller: Chameleon = None
-        pass
 
 
     def init_laser(self):
-
-        address = self.settings.child(('address')).value()
+        address = self.settings.child('address').value()
         initialized = self.controller.open_communication(address)
-
-        self.settings.child(('laser_settings', 'laser_state')).setValue(self.controller.get_laser_state())
-        self.settings.child(('laser_settings', 'laser_wl')).setLimits(self.controller.get_wavelength_range())
-        self.settings.child(('laser_settings', 'laser_wl')).setValue(self.controller.get_wavelength())
-        self.settings.child(('laser_settings', 'laser_power')).setValue(self.controller.get_laser_power())
-        self.settings.child(('laser_settings', 'shutter_state')).setValue(self.controller.get_shutter())
+        self.update_settings_hardware()
 
         return initialized
+
+    def update_settings_hardware(self):
+        self.settings.child('laser_settings', 'laser_state').setValue(self.controller.get_laser_state())
+        self.settings.child('laser_settings', 'laser_wl').setLimits(self.controller.get_wavelength_range())
+        self.settings.child('laser_settings', 'laser_wl').setValue(self.controller.get_wavelength())
+        self.settings.child('laser_settings', 'shutter_state').setValue(self.controller.get_shutter())
+        power = self.controller.get_laser_power()
+        self.settings.child('laser_settings', 'laser_power').setValue(power)
+        align = self.controller.get_align_mode()
+        self.settings.child('laser_settings', 'align', 'align_mode').setValue(align[0])
+        self.settings.child('laser_settings', 'align', 'align_power').setValue(align[1])
+        self.settings.child('laser_settings', 'align', 'align_wl').setValue(align[2])
 
 
     def get_actuator_value(self):
@@ -85,8 +92,8 @@ class DAQ_Move_Chameleon(DAQ_Move_base):
         -------
         float: The position obtained after scaling conversion.
         """
-
         pos = self.controller.get_wavelength()
+        pos = int(round(pos, 0))
         pos = self.get_position_with_scaling(pos)
         return pos
 
@@ -108,23 +115,28 @@ class DAQ_Move_Chameleon(DAQ_Move_base):
         """
 
         if param.name() == 'address':
+            self.controller.close_communication()
             initialized = self.init_laser()
             if not initialized:
                 self.emit_status(ThreadCommand('Update_Status', ['Connection failed']))
 
         elif param.name() == 'laser_state':
-            laser_state = self.settings.child(('laser_settings', 'laser_state')).value()
+            laser_state = self.settings.child('laser_settings', 'laser_state').value()
             self.controller.set_laser_state(laser_state)
 
         elif param.name() == 'laser_wl':
-            wl = self.settings.child(('laser_settings', 'laser_wl')).value()
+            wl = self.settings.child('laser_settings', 'laser_wl').value()
             self.controller.set_wavelength(wl)
+            self.move_done()
 
-        elif param.name() == 'update_power':
-            self.settings.child(('laser_settings', 'laser_power')).setValue(self.controller.get_laser_power())
+        elif param.name() == 'update_settings':
+            self.update_settings_hardware()
+
+        elif param.name() == 'align_mode':
+            self.controller.set_align_mode(param.value())
 
         elif param.name() == 'shutter_state':
-            shutter_state = self.settings.child(('laser_settings', 'shutter_state')).value()
+            shutter_state = self.settings.child('laser_settings', 'shutter_state').value()
             self.controller.set_shutter(shutter_state)
 
         else:
@@ -164,15 +176,15 @@ class DAQ_Move_Chameleon(DAQ_Move_base):
         ----------
         value: (float) value of the absolute target positioning
         """
-
         value = self.check_bound(value)  #if user checked bounds, the defined bounds are applied here
         self.target_value = value
         value = self.set_position_with_scaling(value)  # apply scaling if the user specified one
 
-        self.controller.set_wavelength(value)
+        self.controller.set_wavelength(int(round(value, 0)))
         self.settings.child('laser_settings', 'laser_wl').setValue(self.controller.get_wavelength())
-        self.emit_status(ThreadCommand('Update_Status', ['Wavelength set (absolute)']))
-        self.emit_status(ThreadCommand('move_done'))
+        self.emit_status(ThreadCommand('Update_Status', ['move_abs']))
+
+        self.poll_moving()
 
 
     def move_rel(self, value: DataActuator):
@@ -183,32 +195,27 @@ class DAQ_Move_Chameleon(DAQ_Move_base):
         value: (float) value of the relative target positioning
         """
         value = self.check_bound(self.current_position + value)
-        self.target_value = value
-        value = self.set_position_relative_with_scaling(value)
+        self.target_value = self.current_position + value
+        value = self.set_position_with_scaling(value)
 
-        ## TODO for your custom plugin
-        self.controller.set_wavelength(value)
+        self.controller.set_wavelength_step(int(round(value, 0)))
         self.settings.child('laser_settings', 'laser_wl').setValue(self.controller.get_wavelength())
-        self.emit_status(ThreadCommand('Update_Status', ['Wavelength set (relative)']))
-        self.emit_status(ThreadCommand('move_done'))
+        self.emit_status(ThreadCommand('Update_Status', ['move_rel']))
+
+        self.poll_moving()
 
 
     def move_home(self):
         """Call the reference method of the controller"""
-
-        ## TODO for your custom plugin
-        raise NotImplementedError  # when writing your own plugin remove this line
-        self.controller.your_method_to_get_to_a_known_reference()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        self.controller.home_stepper()
+        self.emit_status(ThreadCommand('Update_Status', ['home']))
 
 
     def stop_motion(self):
         """Stop the actuator and emits move_done signal"""
-
-        ## TODO for your custom plugin
-        raise NotImplementedError  # when writing your own plugin remove this line
-        self.controller.your_method_to_stop_positioning()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        self.close()
+        self.emit_status(ThreadCommand('Update_Status', ['stop_motion']))
+        self.controller.open_communication(self.settings.child('address'))
 
 
 if __name__ == '__main__':
--- src/pymodaq_plugins_coherent/hardware/chameleon.py
+++ src/pymodaq_plugins_coherent/hardware/chameleon.py
@@ -1,25 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from enum import Enum
 import numpy as np
 import pyvisa
+from time import sleep
 
 class Chameleon():
 
     def get_ready_state(self):
         return True
 
-    def open_communication(self, port):
+    def open_communication(self, address):
+        '''
+            Opens VISA communication with Chameleon laser source
 
+            Input:
+                str : VISA address
+
+            Output:
+                None
+        '''
         try:
             rm = pyvisa.ResourceManager()
-            self._device = rm.open_resource(port)
+            print(rm)
+            self._device = rm.open_resource(address)
             self._device.baud_rate = 19200
-            self._device.data_bits = 7
-            self._device.parity = pyvisa.constants.Parity.even
-            self._device.stop_bits = pyvisa.constants.StopBits.one
             self._device.read_termination = '\r\n'
 
+            self._device.clear()
             self._device.query('ECHO=0')
             self._device.query('PROMPT=0')
             initialized = True
@@ -30,56 +37,215 @@ class Chameleon():
         return initialized
 
     def close_communication(self):
+        '''
+            Closes VISA communication with Chameleon laser source
+
+            Input:
+                None
+
+            Output:
+                None
+        '''
         self._device.close()
 
+    def get_SN(self):
+        '''
+            Returns the state of the laser (ON/OFF)
+
+            Input:
+                None
+
+            Output:
+                str : serial number
+        '''
+        return self._device.query('?SN')
 
     def get_laser_state(self):
+        '''
+            Returns the state of the laser (ON/OFF)
+
+            Input:
+                None
+
+            Output:
+                str : laser state ('ON' or 'OFF')
+        '''
         laser = self._device.query('?L')
-        if laser == 0:
+        if laser == '0':
             laser_state = 'OFF'
-        elif laser == 1:
+        elif laser == '1':
             laser_state = 'ON'
         return laser_state
 
     def set_laser_state(self, laser_state):
+        '''
+            Returns the state of the laser (ON/OFF)
+
+            Input:
+                str : laser state ('ON' or 'OFF')
+
+            Output:
+                None
+        '''
         if laser_state == 'OFF':
             laser = 0
         elif laser_state == 'ON':
             laser = 1
-        self._device.write('L={}'.format(laser))
+        self._device.query('L={}'.format(laser))
 
 
     def get_laser_power(self):
+        '''
+            Returns current laser power in mW
+
+            Input:
+                None
+
+            Output:
+                int : laser power
+        '''
         power = self._device.query('?UF')
-        return power
+        return int(power)
 
 
     def get_wavelength_range(self):
+        '''
+            Returns the available wavelength range
+
+            Input:
+                None
+
+            Output:
+                (int,int) : wavelength range (min,max)
+        '''
         wl_min = self._device.query('?TMIN')
         wl_max = self._device.query('?TMAX')
-        return wl_min, wl_max
+        return int(wl_min), int(wl_max)
 
     def get_wavelength(self):
+        '''
+            Returns current wavelength in nm
+
+            Input:
+                None
+
+            Output:
+                int : wavelength in nm
+        '''
         wl = self._device.query('?VW')
-        return wl
+        return int(wl)
 
     def set_wavelength(self, wl):
-        self._device.write('VW={}'.format(wl))
+        '''
+            Sets wavelength in nm
 
+            Input:
+                int : wavelength in nm
+
+            Output:
+                None
+        '''
+        self._device.query('VW={}'.format(wl))
+
+    def set_wavelength_step(self, step):
+        '''
+            Changes wavelength by specified step in nm
+
+            Input:
+                int : wavelength step in nm
+
+            Output:
+                None
+        '''
+        self._device.query('VWS={}'.format(step))
+
+    def home_stepper(self):
+        '''
+            Homes tuning motor
+
+            Input:
+                None
+
+            Output:
+                None
+        '''
+        self._device.query('HM=1')
+
+    def check_tuning_done(self):
+        '''
+            Checks that the laser has finished tuning
+
+            Input:
+                None
+
+            Output:
+                bool : tuning done?
+        '''
+        return self._device.query('?TS') == 0
+
+    def get_align_mode(self):
+        '''
+            Gets alignment mode
+
+            Input:
+                None
+
+            Output:
+                (bool, int, int) : state, available power and wavelength of the alignment mode
+        '''
+        mode = False
+        output = self._device.query('?ALIGN')
+        if output == '1': align_mode = True
+        else: align_mode = False
+        align_power = self._device.query('?ALIGNP')
+        align_wl = self._device.query('?ALIGNW')
+        return align_mode, align_power, align_wl
+
+    def set_align_mode(self, mode):
+        '''
+            Sets alignment mode
+
+            Input:
+                mode : bool
+
+            Output:
+                None
+        '''
+        if mode: output = '1'
+        else: output = '0'
+        self._device.query('ALIGN={}'.format(output))
 
     def get_shutter(self):
+        '''
+            Returns shutter state (OPEN/CLOSED)
+
+            Input:
+                None
+
+            Output:
+                str : shutter state ('OPEN' or 'CLOSED')
+        '''
         shutter = self._device.query('?S')
-        if shutter == 0:
+        if shutter == '0':
             shutter_state = 'CLOSED'
-        elif shutter == 1:
+        elif shutter == '1':
             shutter_state = 'OPEN'
         return shutter_state
 
     def set_shutter(self, shutter_state):
+        '''
+            Sets shutter state (OPEN/CLOSED)
+
+            Input:
+                str : shutter state ('OPEN' or 'CLOSED')
+
+            Output:
+                None
+        '''
         if shutter_state == 'CLOSED':
             shutter = 0
         elif shutter_state == 'OPEN':
             shutter == 1
-        self._device.write('S={}'.format(shutter))
+        self._device.query('S={}'.format(shutter))
 
 
